81bfc1a93b41f5a0c184d7bc20628143dd62ebb8
Derek <derek.forbes77@gmail.com>
2019-11-17 20:19:45 +0900

    added other files

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const app = express()
+const port = 3000
+
+app.use(bodyParser.json())
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+)
+
+app.get('/', (req, res) => {
+    res.json({info: 'Node JS, etc.'})
+})
+
+app.listen(port, () => {
+    console.log(`Port running on ${port}..`);
+})
